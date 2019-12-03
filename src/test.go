d5ffc2e97d59695b1a4752a1a822ee3d1a6be7eb
建鑫鄂 <6503611@qq.com>
2019-12-03 10:14:05 +0800

    src/testgo

--- src/test.go
+++ src/test.go
@@ -1,13 +1,15 @@
 package main
 
+import "fmt"
+
 func add(a int, b int) int {
 	return a + b
 }
 func main() {
 	// var c int
 	// c = add(3, 8)
-	// param := add(5, 15)
+	param := add(5, 15)
 	// fmt.Println("hello word", param)
-	// go fmt.Println("hello word", param)
-	test_pipe()
+	go fmt.Println("hello word", param)
+	// test_pipe()
 }
